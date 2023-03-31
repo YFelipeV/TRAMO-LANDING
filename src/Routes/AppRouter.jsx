5cca8feaaa930e06a53ef5676d22c0f2f0ec2580
Felipe segura <yfsegura04@misena.edu.co>
2023-03-31 07:43:09 -0500

    v.1

---
+++ src/Routes/AppRouter.jsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from "react-router-dom";
+import { Home, About, Contact, Header } from "../Components";
+import { lazy, Suspense } from "react";
+import { Login, Pagina404 } from "../Pages";
+const Head = lazy(() => import("../Components/Header"));
+export const AppRouter = () => {
+  return (
+    <>
+      {/* PUBLICAS*/}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense
+              fallback={
+                <div className="d-flex justify-content-center">
+                  <h4 className="text-white">Cargando...</h4>
+                </div>
+              }
+            >
+              <Head />
+            </Suspense>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contactos" element={<Contact />} />
+        </Route>
+        <Route path="Login" element={<Login />} />
+        <Route path="*" element={<Pagina404 />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
